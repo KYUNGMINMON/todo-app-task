22fccf7ddbe7f1ac581af777d060e304bac8fc59
KYUNGMINMON <rudals7019@gmail.com>
2023-06-12 16:24:55 +0900

    initials

---
+++ src/test/java/com/codestates/todoapptask/TodoAppTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.todoapptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
