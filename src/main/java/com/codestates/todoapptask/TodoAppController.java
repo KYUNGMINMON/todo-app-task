d971f683424ce0a3488e3f81fd9c29dee95f4cb0
KYUNGMINMON <rudals7019@gmail.com>
2023-06-12 16:29:00 +0900

    initials

---
+++ src/main/java/com/codestates/todoapptask/TodoAppController.java
@@ -0,0 +1,12 @@
+package com.codestates.todoapptask;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoAppController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To do Application !";
+    }
+}
